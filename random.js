45106e66ecfe407f6e1599c0e8f79937300fccc4
Konecki <koneckit@emea.roche.com>
2018-10-15 09:32:20 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,13 +1,9 @@
 const generateRandomNumber = () =>
   new Promise((resolve, reject) => {
-    var randomNumber = Math.floor(Math.random() * 10 + 1);
+    let randomNumber = Math.floor(Math.random() * 10 + 1);
     randomNumber <= 5 ? resolve(randomNumber) : reject(randomNumber);
   });
 
 generateRandomNumber()
-  .then(result => {
-    console.log("Success:", result);
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  });
+  .then(result => console.log("Success:", result))
+  .catch(error => console.log("Error:", error));
