63be7c6f051bc8f499c825f6a70e6e8fc021c970
Konecki <koneckit@emea.roche.com>
2018-10-15 09:27:52 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-const products = [{ id: 123, price: 500 }, { id: 456, price: 600 }];
+const products = [
+  { id: 123, price: 500 },
+  { id: 456, price: 600 },
+  { id: 789, price: 1000 }
+];
 
 let id = parseInt(process.argv[2]);
 
 const checkPrice = productId =>
   new Promise((resolve, reject) => {
-    products.map(item => {
-      if (item.id === productId) {
-        resolve(item.price);
-      }
-    });
+    products.map(item => (item.id === productId ? resolve(item.price) : null));
     reject("Could not find item");
   });
 
