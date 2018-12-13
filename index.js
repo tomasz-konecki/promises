b8e83cc1c776fee4d009b5e9d360e6796c5430bd
Konecki <koneckit@emea.roche.com>
2018-12-13 13:04:18 +0100

    Update code

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ window.onload = () => {
   console.log(myGen.next());
   console.log(myGen.next());
 };
+
+// https://www.youtube.com/watch?v=pnS2lBQuLGc&list=PL4cUxeGkcC9jAhrjtZ9U93UMIhnCc44MH&index=5
