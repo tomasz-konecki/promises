900d5fda39d514890446477cfaae6aba608a0eb2
Konecki <koneckit@emea.roche.com>
2018-12-13 12:37:44 +0100

    Update code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # promises
+
 promises
+
+A promise is an object that represents an action that hasn't finished yet,
+but will do at some point down the line.
+It's essentially just a placeholder for the result of some kind of asynchronous
+operation (like a HTTP request).
+
+For example:
+We make a HTTP request for some data. As soon as that asynchronous request is made,
+it returns a promise object straight away, before the data is retrieved and brought
+back to us.
+Within that promise object we can register callbacks, which will run when the request
+completes.
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("this is index.js");
+window.onload = () => {
+  const log = text => console.log(text);
+
+  $.get("data/tweets.json")
+    .then(tweets => {
+      log(tweets);
+      return $.get("data/friends.json");
+    })
+    .then(friends => {
+      log(friends);
+      return $.get("data/videos.json");
+    })
+    .then(videos => log(videos))
+    .catch(err => log(err));
+
+  /*
+  const get = url => {
+    return new Promise((resolve, reject) => {
+      let xhttp = new XMLHttpRequest();
+      xhttp.open("GET", url, true);
+      xhttp.onload = () => {
+        if (xhttp.status == 200) {
+          resolve(JSON.parse(xhttp.response));
+        } else {
+          reject(xhttp.statusText);
+        }
+      };
+
+      xhttp.onerror = () => {
+        reject(xhttp.statusText);
+      };
+      xhttp.send();
+    });
+  };
+
+  let promise = get("data/tweets.json");
+
+  promise
+    .then(tweets => {
+      log(tweets);
+      return get("data/friends.json");
+    })
+    .then(friends => {
+      log(friends);
+      return get("data/videos.json");
+    })
+    .then(videos => log(videos))
+    .catch(error => console.error(error));
+    */
+};
