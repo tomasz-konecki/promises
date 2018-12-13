76f9a8caf4bf8b2d71778b81c8ef3c31e2415f9c
Konecki <koneckit@emea.roche.com>
2018-12-13 12:56:00 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
 window.onload = () => {
-  const log = text => console.log(text);
+  function* gen() {
+    var x = yield 10;
+  }
 
-  // jQuery verion
+  var myGen = gen();
 
-  $.get("data/tweets.json")
-    .then(tweets => {
-      log(tweets);
-      return $.get("data/friends.json");
-    })
-    .then(friends => {
-      log(friends);
-      return $.get("data/videos.json");
-    })
-    .then(videos => log(videos))
-    .catch(err => log(err));
-
-  /*
-  // pure JS version
-
-  const get = url => {
-    return new Promise((resolve, reject) => {
-      let xhttp = new XMLHttpRequest();
-      xhttp.open("GET", url, true);
-      xhttp.onload = () => {
-        if (xhttp.status == 200) {
-          resolve(JSON.parse(xhttp.response));
-        } else {
-          reject(xhttp.statusText);
-        }
-      };
-
-      xhttp.onerror = () => {
-        reject(xhttp.statusText);
-      };
-      xhttp.send();
-    });
-  };
-
-  let promise = get("data/tweets.json");
-
-  promise
-    .then(tweets => {
-      log(tweets);
-      return get("data/friends.json");
-    })
-    .then(friends => {
-      log(friends);
-      return get("data/videos.json");
-    })
-    .then(videos => log(videos))
-    .catch(error => console.error(error));
-    */
+  console.log(myGen.next());
+  console.log(myGen.next());
 };
